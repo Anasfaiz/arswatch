2e797296b1223f9b4bff93daba667a49d58290f1
Anas Faiz <anasfaizzzz@gmail.com>
2026-03-19 20:26:35 +0530

    resolving deployment

--- arswatch/vite.config.js
+++ arswatch/vite.config.js
@@ -1,4 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-export default defineConfig({ plugins:[react(),tailwindcss()] })
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // Use './' to make all paths relative, which is safer for most deployments
+  base: './', 
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
+})
